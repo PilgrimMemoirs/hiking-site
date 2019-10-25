6881393b40c0ca9121a6d85f99ddf0763cf52d76
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2019-10-24 23:44:08 -0700

    generated dashboard controller and deleted greeting

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+    def index
+        @message = "Hello, from dashboard"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'greetings/hello'
-  root 'greetings#hello'
+  root 'dashboard#index'
 end
